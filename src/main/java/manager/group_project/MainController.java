97e7b5e3f3e78792e8424328c171ad7384d92229
maximilian.kaczmarek <maximilian.kaczmarek@cs.ucalgary.ca>
2022-04-06 09:57:27 +0000

    Update controller class

--- src/main/java/manager/group_project/MainController.java
+++ src/main/java/manager/group_project/MainController.java
@@ -1,9 +1,64 @@
 package manager.group_project;
 
+import manager.group_project.*;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainController {
+
+    @FXML
+    private Button addClubButton;
+
+    @FXML
+    private TextField addClubTextfield;
+
+    @FXML
+    private AnchorPane addDataSection;
+
+    @FXML
+    private Button addMatchButton;
+
+    @FXML
+    private TextField awayClubNameTextfield;
+
+    @FXML
+    private TextField awayGoalsTextfield;
+
+    @FXML
+    private AnchorPane getDataSection;
+
+    @FXML
+    private TextField homeClubNameTextfield;
+
+    @FXML
+    private TextField homeGoalsTextfield;
+
+    @FXML
+    private AnchorPane leaderboardArea;
+
+    @FXML
+    private AnchorPane matchLogsArea;
+
+    @FXML
+    private Button removeClubButton;
+
+    @FXML
+    private Button sortButton;
+
+    @FXML
+    private Button viewClubButton;
+
+    @FXML
+    private TextField viewClubNameTextfield;
+
+    @FXML
+    private AnchorPane viewSection;
+    
     void initialze() {
         // TODO implement here
-
     }
 
 }
